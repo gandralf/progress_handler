c35ee981f5d45bb3fa69c3ab4c941ec373662965
Alexandre Gandra Lages <gandra@gmail.com>
2015-05-09 14:28:07 -0300

    Reset progress reporter configuration for each spec

--- spec/progress_reporter_spec.rb
+++ spec/progress_reporter_spec.rb
@@ -4,13 +4,6 @@ describe ProgressReporter do
   let(:items) { 5.times.map &:to_s }
   subject { ProgressReporter.new name: 'Specs', report_gap: 2 }
 
-  let(:observer) { double(:observer, notify_item: nil, notify_progress: nil) }
-  before do
-    ProgressReporter.configure do |config|
-      config.observers = [ observer ]
-    end
-  end
-
   it('has name') { expect(subject.name).to eq 'Specs' }
   it('has a report gap') { expect(subject.report_gap).to eq 2 }
 
@@ -18,6 +11,13 @@ describe ProgressReporter do
     it('yields for each item') { expect {|b| subject.each(items, &b) }.to yield_successive_args(*items) }
 
     describe 'observer notification' do
+      let(:observer) { double(:observer, notify_item: nil, notify_progress: nil) }
+      before do
+        ProgressReporter.configure do |config|
+          config.observers = [ observer ]
+        end
+      end
+
       it('notifies each item') { expect(observer).to receive(:notify_item).with(subject).exactly(items.count).times }
       it('notifies progress') { expect(observer).to receive(:notify_progress).with(subject).exactly(2).times }
       after { subject.each(items) {} }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,9 @@ SPEC_ROOT = File.dirname(__FILE__)
 Dir[File.join(SPEC_ROOT, 'support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.before(:each) do
+    ProgressReporter.configure do |pr_config|
+      pr_config.observers = [ ]
+    end
+  end
 end
