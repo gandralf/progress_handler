9daa7e3afc8c0ad0db0633bfaaa8bd2d7118d2a8
Alexandre Gandra Lages <gandra@gmail.com>
2015-05-09 15:57:55 -0300

    Add console observer

---
+++ lib/progress_reporter/observers/console.rb
@@ -0,0 +1,20 @@
+class ProgressReporter
+  module Observers
+    class Console
+      def notify_item(pr)
+        show_progress(pr) if pr.progress == 1
+        print '.'
+      end
+
+      def notify_progress(pr)
+        show_progress(pr)
+      end
+
+      def show_progress(pr)
+        print "\n#{pr.name}: " if pr.name
+        printf "#{pr.progress} / #{pr.total_size} => %3.1f%%, %3.1f minutes to go ",
+               pr.percent_progress, pr.time_left / 60.0
+      end
+    end
+  end
+end
