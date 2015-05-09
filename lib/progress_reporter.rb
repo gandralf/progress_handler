dc656c2f6e05c10e5eadc7dbcb4a6e13f5e52a3a
Alexandre Gandra Lages <gandra@gmail.com>
2015-05-09 13:36:38 -0300

    Add ProgressReporter#time_left

--- lib/progress_reporter.rb
+++ lib/progress_reporter.rb
@@ -2,30 +2,47 @@ require 'progress_reporter/version'
 require 'progress_reporter/configuration'
 
 class ProgressReporter
-  attr_accessor :name, :report_gap
-  attr_reader :count, :progress, :total_size
+  attr_accessor :name, :report_gap, :total_size
+  attr_reader :count, :progress
 
   def initialize(options)
     @name = options[:name]
     @report_gap = options[:report_gap]
+    reset(options[:total_size])
   end
 
   def each(items)
-    @progress = 0
-    @total_size = items.count
-    @count = 0
-
+    reset(items.count)
     items.each do |item|
-      yield item
-      @count += 1
-
-      notify_observers { |o| o.notify_item(self) }
-
-      if report_gap > 0 and count % report_gap == 0
-        @progress = 100.0 * count / total_size
-        notify_observers { |o| o.notify_progress(self) }
-      end
+      increment { yield item }
     end
+  end
+
+  def increment
+    yield
+    @count += 1
+
+    notify_observers { |o| o.notify_item(self) }
+
+    if report_gap > 0 and count % report_gap == 0
+      @progress = 100.0 * count / total_size
+      notify_observers { |o| o.notify_progress(self) }
+    end
+  end
+
+  def time_left
+    if total_size && count > 0
+      (total_size - count) * ((Time.now - @start_time) / count)
+    end
+  end
+
+  private
+
+  def reset(total_size)
+    @progress = 0
+    @total_size = total_size
+    @count = 0
+    @start_time = Time.now
   end
 
   def notify_observers(&block)
--- spec/progress_reporter_spec.rb
+++ spec/progress_reporter_spec.rb
@@ -1,25 +1,35 @@
 require 'spec_helper'
 
 describe ProgressReporter do
-  describe '#each' do
-    let(:items) { 5.times.map &:to_s }
-    subject { ProgressReporter.new name: 'Specs', report_gap: 2 }
+  let(:items) { 5.times.map &:to_s }
+  subject { ProgressReporter.new name: 'Specs', report_gap: 2 }
 
+  let(:observer) { double(:observer, notify_item: nil, notify_progress: nil) }
+  before do
+    ProgressReporter.configure do |config|
+      config.observers = [ observer ]
+    end
+  end
+
+  describe '#each' do
     it('yields for each item') { expect {|b| subject.each(items, &b) }.to yield_successive_args(*items) }
     it('has name') { expect(subject.name).to eq 'Specs' }
     it('has a report gap') { expect(subject.report_gap).to eq 2 }
 
     describe 'observer notification' do
-      let(:observer) { double(:observer, notify_item: nil, notify_progress: nil) }
-      before do
-        ProgressReporter.configure do |config|
-          config.observers = [ observer ]
-        end
-      end
-
       it('notifies each item') { expect(observer).to receive(:notify_item).with(subject).exactly(items.count).times }
       it('notifies progress') { expect(observer).to receive(:notify_progress).with(subject).exactly(2).times }
       after { subject.each(items) {} }
     end
+  end
+
+  describe '#time_left' do
+    it do
+      subject.total_size = 5
+      subject.increment { sleep 1 }
+      expect(subject.time_left).to be_within(0.1).of(4)
+    end
+    it('is undefined right on start') { expect(subject.time_left).to be_nil }
+    it('is zero at the end') { subject.each(items) {}; expect(subject.time_left).to eq 0 }
   end
 end
