39622abd496cea11a92e25aac8dd68f1c5528e5b
Alexandre Gandra Lages <gandra@gmail.com>
2015-05-10 13:37:24 -0300

    Allow reporter parameters (re)definition in simple let statements

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,7 +1,7 @@
 module Helpers
-  def setup_reporter_spec(reporter, reporter_options = {})
+  def setup_reporter_spec(reporter)
     before { ProgressHandler.configure {|config| config.reporters = {reporter => reporter_options} } }
-
+    let(:reporter_options) { {} }
     let(:items) { 5.times.map &:to_s }
     let(:progress_handler) { ProgressHandler.new name: 'Specs', report_gap: 2 }
   end
